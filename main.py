cf4c68a3bfee1b22c2bbf9d692ff51f3699c7b7e
Erangoud <egoud83@gmail.com>
2024-12-17 14:52:07 +0530

    added the die which roll's between the 1&6

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#use case 1:- player starts at position 0 
-
+#use case 2:- rolls the die to get a number between 1 to 6. 
+ 
+import random
 def start_game():
     player_position = 0
     rolls_count = 0
-    
+def roll_die():
+    return random.randint(1, 6)
